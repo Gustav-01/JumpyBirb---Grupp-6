5a8d9a8d7ecec8d001c1d6d10669f57fbbc9b395
Gustav O <ac75309@skola.goteborg.se>
2026-03-17 15:44:59 +0100

    Comments removed

--- core/src/main/java/se/yrgo/game/BirbGame.java
+++ core/src/main/java/se/yrgo/game/BirbGame.java
@@ -1,24 +1,12 @@
 package se.yrgo.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 
 /**
  * The main game class, containing application life-cycle methods.
  */
 public class BirbGame extends Game {
-
-
-//    private AssetManager assetManager;
-//    private SpriteBatch batch;
-
 
     private GameScreen gameScreen;
 
@@ -30,29 +18,6 @@ public class BirbGame extends Game {
     public void create() {
         gameScreen = new GameScreen(this);
         setScreen(gameScreen);
-//        assetManager = new AssetManager();
-//        batch = new SpriteBatch();
-//
-//        assetManager.load("birb_the_kiwi.png", Texture.class);
-//
-//        assetManager.load("Test kiwi no wings-1.png", Texture.class);
-//
-//        assetManager.finishLoading();
-//
-//        smallKiwi = new Sprite(assetManager.get("Test kiwi no wings-1.png", Texture.class));
-//        smallKiwi.setSize(1, 1); //Todo: recalculate
-//        smallKiwi.setPosition(1,1);
-//
-//        kiwi = new Sprite(assetManager.get("birb_the_kiwi.png", Texture.class));
-//        kiwi.setSize(4.8f, 3.2f);
-//        kiwi.setPosition(8, 8);
-//
-//        camera = new OrthographicCamera();
-//        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
-//
-//        camera.position.set(WORLD_WIDTH, WORLD_HEIGHT, 0);
-//        camera.update();
-
     }
 
     /**

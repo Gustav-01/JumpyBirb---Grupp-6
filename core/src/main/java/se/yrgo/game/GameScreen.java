0bf6ae8f4237b605a09c7a3ee394a578f0f90768
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-03-17 14:54:52 +0100

    Moving Screen ralated Game logic to GameScreen

--- core/src/main/java/se/yrgo/game/GameScreen.java
+++ core/src/main/java/se/yrgo/game/GameScreen.java
@@ -2,12 +2,26 @@ package se.yrgo.game;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen implements Screen {
+    private static final int WORLD_WIDTH = 800;
+    private static final int WORLD_HEIGHT = 600;
+
 
     private final BirbGame game;
+    private SpriteBatch batch;
+    private Sprite kiwi;
+//    private Sprite smallKiwi;
+
+    private Viewport viewport;
+    private Camera camera;
 
     private Texture background;
     private float bgX1 = 0;
@@ -17,44 +31,86 @@ public class GameScreen implements Screen {
     public GameScreen(BirbGame game) {
         this.game = game;
 
-        background = new Texture("background.png");
+        batch = new SpriteBatch();
 
-        // Placera två kopior av bakgrunden bredvid varandra
-        bgX2 = background.getWidth();
+        kiwi = new Sprite(new Texture("Kiwi_wing_up.png"));
+        kiwi.setSize(153.6f, 102.4f);
+        kiwi.setPosition(WORLD_WIDTH / 5, WORLD_HEIGHT / 2);
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
+        camera.position.set(WORLD_WIDTH, WORLD_HEIGHT, 0);
+        camera.update();
+//
+//        background = new Texture("background.png");
+//
+//        // Placera två kopior av bakgrunden bredvid varandra
+//        bgX2 = background.getWidth();
     }
 
     @Override
     public void render(float delta) {
+        draw();
 
         // Uppdatera bakgrundens position (åt höger)
-        bgX1 += bgSpeed * delta;
-        bgX2 += bgSpeed * delta;
-
-        // Looping åt höger
-        if (bgX1 >= background.getWidth()) {
-            bgX1 = bgX2 - background.getWidth();
-        }
-        if (bgX2 >= background.getWidth()) {
-            bgX2 = bgX1 - background.getWidth();
-        }
-
-        // Rensa skärmen
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        bgX1 += bgSpeed * delta;
+//        bgX2 += bgSpeed * delta;
+//
+//        // Looping åt höger
+//        if (bgX1 >= background.getWidth()) {
+//            bgX1 = bgX2 - background.getWidth();
+//        }
+//        if (bgX2 >= background.getWidth()) {
+//            bgX2 = bgX1 - background.getWidth();
+//        }
+//
+//        // Rensa skärmen
+//        Gdx.gl.glClearColor(0, 0, 0, 1);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // Rita bakgrunden
-        game.batch.begin();
-        game.batch.draw(background, bgX1, 0);
-        game.batch.draw(background, bgX2, 0);
-        game.batch.end();
+//        batch.begin();
+//        batch.draw(background, bgX1, 0);
+//        batch.draw(background, bgX2, 0);
+
+
+//        batch.end();
 
     }
 
-    @Override public void resize(int width, int height) {}
-    @Override public void show() {}
-    @Override public void hide() {}
-    @Override public void pause() {}
-    @Override public void resume() {}
+    public void draw() {
+        ScreenUtils.clear(Color.GRAY);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        batch.begin();
+
+        kiwi.draw(batch);
+
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
 
     @Override
     public void dispose() {

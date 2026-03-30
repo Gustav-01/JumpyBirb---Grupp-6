0329ffe9808f9153e98fe4c59e8a8c11ba144962
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-03-30 20:03:20 +0200

    Making quick example code for displaying game over screen, for testing purposes

--- core/src/main/java/se/yrgo/game/GameOverScreen.java
+++ core/src/main/java/se/yrgo/game/GameOverScreen.java
@@ -1,23 +1,54 @@
 package se.yrgo.game;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+import se.yrgo.game.constants.GameFunctionalityConstants;
 
 public class GameOverScreen implements Screen {
     private BirbGame game;
 
+    private BitmapFont bigFont;
+    private BitmapFont smallFont;
+    private SpriteBatch batch = new SpriteBatch();
+
     public GameOverScreen(BirbGame game) {
         this.game = game;
+
+        this.bigFont = new BitmapFont();
+        final Color fontColor = Color.WHITE;
+        this.bigFont.setColor(fontColor);
+
+        this.bigFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        this.bigFont.getData().setScale(2.5f);
+
+        this.smallFont = new BitmapFont();
+        this.smallFont.setColor(fontColor);
+        this.smallFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        this.smallFont.getData().setScale(1.5f);
     }
 
 
     @Override
     public void show() {
-        //Popup a screen
     }
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(Color.GRAY);
 
+        batch.begin();
+        bigFont.draw(batch, "Game Over!",
+            0,
+            GameFunctionalityConstants.WORLD_HEIGHT / 2f,
+            600,
+            Align.center,
+            false);
+        batch.end();
     }
 
     @Override
@@ -42,6 +73,8 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        bigFont.dispose();
+        smallFont.dispose();
     }
 }

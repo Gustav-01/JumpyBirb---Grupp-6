9f4560fa7c42b8b58a787e1b6e1efe20b4ae1612
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-20 18:15:33 +0200

    Adding background image for game over screen, and changing font color to match kiwi green

--- core/src/main/java/se/yrgo/game/screens/GameOverScreen.java
+++ core/src/main/java/se/yrgo/game/screens/GameOverScreen.java
@@ -3,12 +3,17 @@ package se.yrgo.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import se.yrgo.game.BirbGame;
 import se.yrgo.game.constants.GameFunctionalityConstants;
 
@@ -20,11 +25,16 @@ public class GameOverScreen implements Screen {
     private SpriteBatch batch = new SpriteBatch();
     private int currentScore;
 
+    private Sprite background;
+
+    private Viewport viewport;
+    private Camera camera;
+
     public GameOverScreen(BirbGame game, int currentScore) {
         this.game = game;
 
         this.bigFont = new BitmapFont();
-        final Color fontColor = Color.WHITE;
+        final Color fontColor = new Color(0.494f, 0.788f, 0.0039f, 1.0f);
         this.bigFont.setColor(fontColor);
 
         this.bigFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
@@ -35,6 +45,12 @@ public class GameOverScreen implements Screen {
         this.smallFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         this.smallFont.getData().setScale(1.5f);
 
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(GameFunctionalityConstants.WORLD_WIDTH, GameFunctionalityConstants.WORLD_HEIGHT, camera);
+
+        this.background = new Sprite(new Texture("gameover_bg_small.png"));
+        background.setSize(GameFunctionalityConstants.WORLD_WIDTH, GameFunctionalityConstants.WORLD_HEIGHT);
+
         this.currentScore = currentScore;
 
     }
@@ -42,27 +58,33 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void show() {
+        background.setPosition(0, 0);
+        camera.position.set(GameFunctionalityConstants.WORLD_WIDTH, GameFunctionalityConstants.WORLD_HEIGHT, 0);
+        camera.update();
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.GRAY);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
 
         batch.begin();
 
-        float worldWidth = GameFunctionalityConstants.WORLD_WIDTH;
+        background.draw(batch);
+
         float worldHeight = GameFunctionalityConstants.WORLD_HEIGHT;
+        float worldWidth = GameFunctionalityConstants.WORLD_WIDTH;
 
         float centerX = 0;
-        float width = worldWidth;
 
         float centerY = worldHeight / 2f;
 
         // Game Over text
         bigFont.draw(batch, "Game Over!",
             centerX,
-            centerY + 100,
-            width,
+            centerY + 200,
+            worldWidth,
             Align.center,
             false
         );
@@ -70,15 +92,15 @@ public class GameOverScreen implements Screen {
         // Score
         smallFont.draw(batch, "Score: " + currentScore,
             centerX,
-            centerY + 30,
-            width,
+            centerY + 60,
+            worldWidth,
             Align.center,
             false
         );
         smallFont.draw(batch, "Personal best: " + game.getPreviousHighscore(),
             centerX,
-            centerY + 60,
-            width,
+            centerY + 120,
+            worldWidth,
             Align.center,
             false
         );
@@ -86,8 +108,8 @@ public class GameOverScreen implements Screen {
         // Restart instruction
         smallFont.draw(batch, "Press SPACE to restart",
             centerX,
-            centerY - 40,
-            width,
+            centerY - 10,
+            worldWidth,
             Align.center,
             false
         );
@@ -104,7 +126,7 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -127,5 +149,6 @@ public class GameOverScreen implements Screen {
         batch.dispose();
         bigFont.dispose();
         smallFont.dispose();
+        background.getTexture().dispose();
     }
 }

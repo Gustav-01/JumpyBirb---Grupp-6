bc066d4cef728eb527688bde71d5427ba805454e
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-28 21:07:13 +0200

    Adding scorebox texture for scores

--- core/src/main/java/se/yrgo/game/constants/GameFunctionalityConstants.java
+++ core/src/main/java/se/yrgo/game/constants/GameFunctionalityConstants.java
@@ -8,4 +8,7 @@ public class GameFunctionalityConstants {
     public static final int WORLD_WIDTH = 800;
     public static final int WORLD_HEIGHT = 600;
     public static final float GRAVITY = -900f;
+
+    public static final float SCORE_BOX_WIDTH = 720 / 3f;
+    public static final float SCORE_BOX_HEIGHT = 360 / 3f;
 }
